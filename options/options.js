0203f0a817231cc0c05b3a8ff2f74433880a3956
Crowdbotics <team@crowdbotics.com>
2021-08-06 07:35:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "snowy_breeze_29551Identifier",
+  url: "https://snowy_breeze_29551Identifier.botics.co",
+  api: "https://snowy_breeze_29551Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
